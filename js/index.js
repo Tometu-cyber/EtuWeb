1dabb4858ba6fc4ffb104327a063afe02c34b2e2
Tometu-cyber <146548356+Tometu-cyber@users.noreply.github.com>
2025-07-07 23:03:12 +0200

    Depot de la version docker

--- js/index.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js')
-    .then(reg => console.log('Service worker enregistré', reg))
-    .catch(err => console.error('Erreur SW', err));
-}
--- js/sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
-self.addEventListener('fetch', event => {
-  // Mode pass-through, tu peux améliorer pour le offline
-});
